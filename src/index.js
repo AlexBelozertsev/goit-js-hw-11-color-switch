77fe0a5112db26f66c9b7486126ae785aad24d3a
AlexBelozertsev <kashim983@gmail.com>
2020-12-21 19:36:54 +0200

    добавил функции активации атрибутов кнопки

--- src/index.js
+++ src/index.js
@@ -5,25 +5,22 @@ const stopBtn = document.querySelector('button[data-action="stop"]');
 const body = document.querySelector('body');
 
 const colors = [
-  '#FFFFFF',
-  '#2196F3',
-  '#4CAF50',
-  '#FF9800',
-  '#009688',
-  '#795548',
+    '#FFFFFF',
+    '#2196F3',
+    '#4CAF50',
+    '#FF9800',
+    '#009688',
+    '#795548',
 ];
+
+activateAttributeBtn(startBtn, 'enabled');
 
 const interval = {
     intervalId: null,
-    isActive: false,
-    enabled: startBtn.setAttribute('dataset', 'enabled'),
 
     start() {
-        if (this.isActive) {
-            return;
-        };
-        this.enabled = startBtn.setAttribute('dataset', 'disabled');
-        this.isActive = true;
+        deactAttrBtn(startBtn, 'enabled');
+        activateAttributeBtn(startBtn, 'disabled');
         this.intervalId = setInterval(() => {
             body.style.backgroundColor = `${colors[randomIntegerFromInterval(0, colors.length-1)]}`;
         }, 1000);
@@ -31,13 +28,20 @@ const interval = {
     stop() {
         clearInterval(this.intervalId);
         this.intervalId = null;
-        this.isActive = false;
-        this.enabled;
-
+        deactAttrBtn(startBtn, 'disabled');
+        activateAttributeBtn(startBtn, 'enabled');
+        ;
     },
 };
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+function activateAttributeBtn(btn, value) {
+    return btn.setAttribute(value, '')
+};
+function deactAttrBtn(btn, value) {
+    return btn.removeAttribute(value)
 };
 
 startBtn.addEventListener('click', interval.start.bind(interval));
